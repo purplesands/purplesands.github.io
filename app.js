cca7fbeda5acbb6268334c026df50d7c4a9afb82
purplesands <mullenjoe13@gmail.com>
2019-05-08 16:50:57 -0400

    yt resizing

--- app.js
+++ app.js
@@ -4,17 +4,28 @@ document.addEventListener('DOMContentLoaded', e=> {
   const links = document.querySelector('.links')
   const video = document.querySelector('.video')
 
-
   document.body.addEventListener("click", e=>{
     if (e.target.classList.contains("lemonDemo")) {
-      video.innerHTML = `<button class="closeOut">x</button>
-      <iframe width="675" height="425" src="https://www.youtube.com/embed/cOYl9kt5umA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+      video.innerHTML = `
+      <div class="container">
+        <button class="closeOut">x</button>
+        <iframe src="https://www.youtube.com/embed/cOYl9kt5umA"
+        frameborder="0" allowfullscreen class="video2"></iframe>
+      </div>`
     } else if (e.target.classList.contains("tetDemo")) {
-      video.innerHTML = `<button class="closeOut">x</button>
-    <iframe width="675" height="370" src="https://www.youtube.com/embed/fA6uFaQ50_I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+      video.innerHTML = `
+      <div class="container">
+        <button class="closeOut">x</button>
+        <iframe src="https://www.youtube.com/embed/fA6uFaQ50_I"
+        frameborder="0" allowfullscreen class="video2"></iframe>
+      </div>`
   } else if (e.target.classList.contains("birds")) {
-    video.innerHTML = `<button class="closeOut">x</button>
-    <iframe width="600" height="420" src="https://www.youtube.com/embed/TVmV8UHz-YM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+    video.innerHTML = `
+      <div class="container">
+        <button class="closeOut">x</button>
+        <iframe src="https://www.youtube.com/embed/TVmV8UHz-YM"
+        frameborder="0" allowfullscreen class="video2"></iframe>
+      </div>`
 
   }
     document.querySelector('.closeOut').addEventListener("click", e=> {
