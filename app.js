85aff0fb7c185f32e5fb287ffa5530e3aff4e152
purplesands <mullenjoe13@gmail.com>
2019-05-08 16:12:04 -0400

    commented out in progress project

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', e=> {
     <iframe width="675" height="370" src="https://www.youtube.com/embed/fA6uFaQ50_I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
   } else if (e.target.classList.contains("birds")) {
     video.innerHTML = `<button class="closeOut">x</button>
-    <iframe width="600" height="450" src="https://www.youtube.com/embed/TVmV8UHz-YM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+    <iframe width="600" height="420" src="https://www.youtube.com/embed/TVmV8UHz-YM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
 
   }
     document.querySelector('.closeOut').addEventListener("click", e=> {
