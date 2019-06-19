e0168902c05fa1f112347c5f43c590001173bc4d
purplesands <mullenjoe13@gmail.com>
2019-06-19 13:49:06 -0400

    redesign

--- app.js
+++ app.js
@@ -1,37 +1,61 @@
+$(document).ready(function(){
 
-document.addEventListener('DOMContentLoaded', e=> {
+            // var scroll_pos = 0;
+            // $(document).scroll(function() {
+            //
+              // var distanceFromTop = $(document).scrollTop();
+              // console.log(distanceFromTop)
+              //
+              //   if(distanceFromTop < 138 )
+              //   {
+              //       let num = 1-$(document).scrollTop()/138
+              //       console.log(num)
+              //       // reduce div height to 250px
+              //       $('.head').css('transform', 'scale(1, '+num+')');
+              //   }
+              //   else
+              //   {
+              //       // set minimum div height to 100px
+              //       $('.head').css('transform', 'scale(1, 0.5)');
+              //
+              //   }
+              //
+                // scroll_pos = $(this).scrollTop();
+                // if(scroll_pos > 90) {
+                //     $(".head").css({'background-color':'transparent', 'opacity':'0.7', 'transform': 'scale(1, 0.5)','top':'-2em'});
+                //
+                // } else {
+                //     $(".head").css({'background-color':'transparent', 'opacity':'1', 'transform': 'scale(1, 1)','top':'0.5em'});
+                // }
+            // });
+            // $(".popup").click(function () {
+            //     var $this = $(this);
+            //     var $iframe = $("<iframe>").attr("src", $this.data("link")).css({"width": 400, "height": 300});
+            //     var $title = $("<h1>").text($this.data("title"));
+            //     $("#video-view").html($title).append($iframe);
+            //     $iframe.wrap("<div class='class-video'>");
+            // });
 
-  const links = document.querySelector('.links')
-  const video = document.querySelector('.video')
+            $('.hoke').hover(function () {
+                $("#hoke").show();
+                $("#sel").hide();
+            },function () {
+                $("#hoke").hide();
+                $("#sel").show();
 
-  document.body.addEventListener("click", e=>{
-    if (e.target.classList.contains("lemonDemo")) {
-      video.innerHTML = `
-      <div class="container">
-        <button class="closeOut">x</button>
-        <iframe src="https://www.youtube.com/embed/cOYl9kt5umA"
-        frameborder="0" allowfullscreen class="video2"></iframe>
-      </div>`
-    } else if (e.target.classList.contains("tetDemo")) {
-      video.innerHTML = `
-      <div class="container">
-        <button class="closeOut">x</button>
-        <iframe src="https://www.youtube.com/embed/fA6uFaQ50_I"
-        frameborder="0" allowfullscreen class="video2"></iframe>
-      </div>`
-  } else if (e.target.classList.contains("birds")) {
-    video.innerHTML = `
-      <div class="container">
-        <button class="closeOut">x</button>
-        <iframe src="https://www.youtube.com/embed/TVmV8UHz-YM"
-        frameborder="0" allowfullscreen class="video2"></iframe>
-      </div>`
-
-  }
-    document.querySelector('.closeOut').addEventListener("click", e=> {
-      video.innerHTML = ""
-    })
-
-  })
-
-})
+            });
+            $('.lemontown').hover(function () {
+                $("#lemontown").show();
+                $("#sel").hide();
+            },function () {
+                $("#lemontown").hide();
+                $("#sel").show();
+            });
+            $('.tetrotis').hover(function () {
+                $("#tetrotis").show();
+                $("#sel").hide();
+            },function () {
+                $("#tetrotis").hide();
+                $("#sel").show();
+            });
+        });
